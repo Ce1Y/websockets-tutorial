5419a19a2d4ebb280be1673bc21ae8d425d9d8a0
Ce1Y <sycn02430@gmail.com>
2023-04-18 15:02:51 +0800

    Initial implementation.

--- app.py
+++ app.py
@@ -3,7 +3,9 @@
 import asyncio
 import itertools
 import json
+import os
 import secrets
+import signal
 
 import websockets
 
@@ -194,56 +196,17 @@ async def handler(websocket):
         # First player starts a new game.
         await start(websocket)
 
-# async def handler(websocket):
-#     # Initialize a Connect Four game.
-#     game = Connect4()
-
-#     # Players take alternate turns, using the same browser.
-#     turns = itertools.cycle([PLAYER1, PLAYER2])
-#     player = next(turns)
-
-#     async for message in websocket:
-#         # Parse a "play" event from the UI.
-#         event = json.loads(message)
-#         assert event["type"] == "play"
-#         column = event["column"]
-
-#         try:
-#             # Play the move.
-#             row = game.play(player, column)
-#         except RuntimeError as exc:
-#             # Send an "error" event if the move was illegal.
-#             event = {
-#                 "type": "error",
-#                 "message": str(exc),
-#             }
-#             await websocket.send(json.dumps(event))
-#             continue
-
-#         # Send a "play" event to update the UI.
-#         event = {
-#             "type": "play",
-#             "player": player,
-#             "column": column,
-#             "row": row,
-#         }
-#         await websocket.send(json.dumps(event))
-
-#         # If move is winning, send a "win" event.
-#         if game.winner is not None:
-#             event = {
-#                 "type": "win",
-#                 "player": game.winner,
-#             }
-#             await websocket.send(json.dumps(event))
-
-#         # Alternate turns.
-#         player = next(turns)
-
 
 async def main():
-    async with websockets.serve(handler, "", 8001):
-        await asyncio.Future()  # run forever
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.ass_signal_handler(signal.SIGTERM, stop.set_result, None)
+    
+    port = int(os.environ.get("PORT", "8001"))
+    async with websockets.serve(handler, "", port):
+        # await asyncio.Future()  # run forever
+        await stop
 
 
 if __name__ == "__main__":
